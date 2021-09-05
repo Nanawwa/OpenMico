0b959b5d26fcf0d511767dd850d4cfee31fc49db
Aizawa Hikaru <37921907+azwhikaru@users.noreply.github.com>
2021-09-06 01:14:50 +0800

    Create BuildConfig.java

---
+++ OpenMico-FOTA_resign/BuildConfig.java
@@ -0,0 +1,12 @@
+// From user-relesae 1.24.9
+
+package com.xiaomi.mico.romupdate;
+
+public final class BuildConfig {
+    public static final String APPLICATION_ID = "com.xiaomi.mico.romupdate";
+    public static final String BUILD_TYPE = "release"; // Update channel
+    public static final boolean DEBUG = false;
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = 2;
+    public static final String VERSION_NAME = "1.1";
+}
